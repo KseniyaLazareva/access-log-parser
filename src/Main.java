ea4bae07f753b2202f910c29069ceb6fbfa493b3
Kseniya Lazareva <KLazareva@inno.tech>
2026-01-24 20:05:06 +0300

    add Cycle with scanner

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -5,5 +6,27 @@ public class Main {
         System.out.println("Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());
+
+        Scanner scanner = new Scanner(System.in);
+        int countValidPatch = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу");
+            String path = scanner.nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (isDirectory == true && fileExists == true) {
+                System.out.println("Указана директория, а не файл. Попробуйте снова.");
+            } else if (fileExists == false && isDirectory == false) {
+                System.out.println("Файл не существует. Попробуйте снова.");
+                continue;
+            }
+            if (fileExists == true && isDirectory == false) {
+                countValidPatch++;
+                System.out.println("Путь указан верно, это файл " + countValidPatch);
+
+            }
+        }
     }
 }
+
