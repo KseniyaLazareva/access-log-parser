ed40c37961717446384b73d674b0eaef944848b2
Kseniya Lazareva <KLazareva@inno.tech>
2026-02-28 15:16:46 +0300

    add separation string

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());
-        long lineCount = 1;
+        long lineCount;
         int maxLength = 0;
-        int minLength = 0;
-
+        int minLength = maxLength;
+        int countYandexBot;
+        int countGooglebot;
+        double ratioGooglebot;
+        double ratioYandexBot;
         Scanner scanner = new Scanner(System.in);
         int countValidPatch = 0;
         while (true) {
+            lineCount = 0;
+            countYandexBot = 0;
+            countGooglebot = 0;
             System.out.println("Введите путь к файлу");
             String path = scanner.nextLine();
             File file = new File(path);
@@ -43,15 +51,55 @@ public class Main {
                         if (line.length() > 1024) {
                             throw new LongLineException("Строка в файле длиннее 1024 символов.");
                         }
+                        String regex = "^(\\S+) (\\S+) (\\S+) \\[(.+?)\\] \"(.+?)\" (\\d+) (\\d+) \"(.*?)\" \"(.*?)\"$";
+                        Pattern pattern = Pattern.compile(regex);
+                        Matcher matcher = pattern.matcher(line);
+                        if (matcher.find()) {
+                            String ip = matcher.group(1);
+                            String property1 = matcher.group(2);
+                            String property2 = matcher.group(3);
+                            String date = matcher.group(4);
+                            String request = matcher.group(5);
+                            String status = matcher.group(6);
+                            String size = matcher.group(7);
+                            String referrer = matcher.group(8);
+                            String userAgent = matcher.group(9);
+                            int startIdx = userAgent.indexOf('(');
+                            int endIdx = userAgent.indexOf(')', startIdx);
+                            if (startIdx != -1 && endIdx != -1) {
+                                String insideParentheses = userAgent.substring(startIdx + 1, endIdx);
+                                String[] parts = insideParentheses.split(";");
+                                if (parts.length >= 2) {
+                                    String fragment = parts[1].trim();
+                                    int slashIdx = fragment.indexOf('/');
+                                    String partBeforeSlash;
+                                    if (slashIdx != -1) {
+                                        partBeforeSlash = fragment.substring(0, slashIdx).trim();
+                                        if (partBeforeSlash.equals("Googlebot")) {
+                                            countGooglebot++;
+                                        }
+                                        if (partBeforeSlash.equals("YandexBot")) {
+                                            countYandexBot++;
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            System.out.println("Не удалось распарсить строку");
+                        }
                     }
                     System.out.println("Общее количество строк в файле " + lineCount);
-                    System.out.println("Длина самой длинной строки в файле " + maxLength);
-                    System.out.println("Длина самой короткой строки в файле " + minLength);
-
+                    if (lineCount > 0) {
+                        ratioYandexBot = (double) countYandexBot / lineCount;
+                        System.out.printf("Доля запросов с YandexBot: %.2f%%\n", ratioYandexBot * 100);
+                    }
+                    if (lineCount > 0) {
+                        ratioGooglebot = (double) countGooglebot / lineCount;
+                        System.out.printf("Доля запросов с Googlebot: %.2f%%\n", ratioGooglebot * 100);
+                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
                 System.out.println("Путь указан верно, это файл " + countValidPatch);
                 continue;
             }
@@ -59,6 +107,3 @@ public class Main {
         }
     }
 }
-
-
-
